a7e9cbf6908d301f190231595c1732f6bc37eef2
TheJulius <juliocswin@gmail.com>
2021-12-17 21:39:38 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,25 +1,3 @@
-# Laravel REST API with Sanctum
-
-This is an example of a REST API using auth tokens with Laravel Sanctum
-
-## Usage
-
-Change the *.env.example* to *.env* and add your database info
-
-For SQLite, add
-```
-DB_CONNECTION=sqlite
-DB_HOST=127.0.0.1
-DB_PORT=3306
-```
-
-Create a _database.sqlite_ file in the _database_ directory
-
-```
-# Run the webserver on port 8000
-php artisan serve
-```
-
 ## Routes
 
 ```
--- routes/api.php
+++ routes/api.php
@@ -5,19 +5,6 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-// Route::resource('products', ProductController::class);
-
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
